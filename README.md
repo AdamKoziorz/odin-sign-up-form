ca6978bf477ca54bef970b116ad55125362ca664
AdamKoziorz <adam-koziorz@outlook.com>
2023-06-18 16:00:59 -0400

    Implement Validation and Fix Styling
    The "eager" validation approach has been implemented and the form functions and looks as it is supposed to. Additional improvements and features could be made, but this should be sufficient for the purpose of this project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# odin-sign-up-form
+
+I am completing various small projects as part of the Odin Project. "Sign-Up Form" is the first project in the Full-stack Javascript course and the first project in the intermediate HTML/CSS course. This project tests form creation and form validation as well as intermediate CSS concepts such as positioning, units, functions, and advanced selectors.
+
+This site is responsive for desktop but not for mobile as this has not been covered yet in the curriculum.
---
+++ script.js
@@ -0,0 +1,110 @@
+const form = document.getElementById('sign-up-form');
+const password = document.getElementById('password');
+const confirmpassword = document.getElementById('confirm-password');
+const info = document.querySelector('.info');
+info.disabled = true;
+form.noValidate = true;
+form.addEventListener('submit', validateForm);
+
+
+// Simple function to invalidate an input box (message + styling)
+function invalidate(input) {
+    input.classList.add("invalid");
+    input.style.borderBottom = `2px solid red`;
+    if (input.value === "") {
+        input.nextElementSibling.textContent = "This is a required field";
+    } else if (input.id === "email") {
+        input.nextElementSibling.textContent = "Email is not in a valid format (aaa@aaa.aaa)";
+    } else if (input.id === "phone") {
+        input.nextElementSibling.textContent = "Phone # is not in a valid format (xxx-xxx-xxxx)";
+    } else if ((input.id === "password") || (input.id === "confirm-password")) {
+        input.nextElementSibling.textContent = "Passwords do not match";
+    }
+}
+
+
+// Simple function to validate an input box (message + styling)
+function validate(input) {
+    input.nextElementSibling.textContent = '\u200B';
+    input.style.borderBottom = `2px solid green`;
+}
+
+
+// Wrapper function for general validation
+function validateText(input) {
+    if (!input.checkValidity()) {
+        invalidate(input);
+    } else {
+        if ((input === password) || (input === confirmpassword)) {
+            validatePasswords(input);
+            return;
+        }
+        validate(input);
+    }
+}
+
+
+// Specific wrapper function for validating passwords
+function validatePasswords(input) {
+    if ((password.value !== "") && (confirmpassword.value !== "")) {
+        if (password.value !== confirmpassword.value) {
+            invalidate(password);
+            invalidate(confirmpassword);
+        } else {
+            validate(password);
+            validate(confirmpassword);
+        }
+    } else if (input.value !== "") {
+        validate(input);
+    }
+}
+
+
+// Validates the form on submission
+function validateForm(e) {
+    const form = e.target;
+    if (form.checkValidity()) {
+        // Form is valid, but we need to check passwords
+        if (password.value !== confirmpassword.value) {
+            e.preventDefault();
+            invalidate(password);
+            invalidate(confirmpassword);
+        } else {
+            // All inputs are correct
+            alert("Thanks for signing up! However, this is not an actual site...");
+        }
+    } else {
+        // Get which inputs are invalid
+        e.preventDefault();
+        Array.from(form.elements).forEach(i => { 
+            validateText(i);
+         });
+    }
+}
+
+
+// We're going to implement an "eager" validation approach using
+// event listeners on each input and validation wrappers 
+const inputList = document.querySelectorAll("input");
+inputList.forEach(input => {
+    // When we leave text input, we validate "lazily"
+    input.addEventListener("blur", () => {
+        // We have to consider passwords separately
+        if ((input === password) || (input === confirmpassword)) {
+            validatePasswords(input);
+            return;
+        }
+        if (input.checkValidity()) {
+            validate(input);
+        } else if (input.value !== "") {
+            invalidate(input);
+        }
+    });
+
+    // If some input is invalid, we validate "aggressively"
+    input.addEventListener("input", () => {
+        if (input.classList.contains("invalid")) {
+            validateText(input);
+        }
+    })
+});
